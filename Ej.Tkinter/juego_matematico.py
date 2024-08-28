fc44a7fc7aabf2ef116865fd345c8512f21b5465
Benjamin Briolini <benjamin.briolini@gmail.com>
2024-08-28 15:17:49 -0300

    Update juego_matematico.py

--- Ej.Tkinter/juego_matematico.py
+++ Ej.Tkinter/juego_matematico.py
@@ -1,23 +1,29 @@
 import tkinter as tk
+from tkinter import messagebox
 import random
 
 ventana = tk.Tk()
 ventana.title("Juego Matemático")
 ventana.geometry("300x300")
+ventana.config(bg="lightblue")
 
-signo_label = tk.Label(ventana,text="?")
+cont = 0
+buenas = 0
+malas = 0
+
+signo_label = tk.Label(ventana,text="?",bg="lightblue")
 signo_label.grid(row=0, column=1, padx = 10, pady=25)
-juego_label = tk.Label(ventana,text="Juegos:")
+juego_label = tk.Label(ventana,text="Juegos:",bg="lightblue")
 juego_label.place(x=200,y=170)
-buenos_label = tk.Label(ventana,text="Buenos:")
+buenos_label = tk.Label(ventana,text="Buenos:",bg="lightblue")
 buenos_label.place(x=200,y=200)
-malos_label = tk.Label(ventana,text="Malos:")
+malos_label = tk.Label(ventana,text="Malos:",bg="lightblue")
 malos_label.place(x=200,y=230)
-numjuego_label = tk.Label(ventana,text="")
+numjuego_label = tk.Label(ventana,text="",bg="lightblue")
 numjuego_label.place(x=260,y=170)
-numbueno_label = tk.Label(ventana,text="")
+numbueno_label = tk.Label(ventana,text="",bg="lightblue")
 numbueno_label.place(x=260,y=200)
-nummalo_label = tk.Label(ventana,text="")
+nummalo_label = tk.Label(ventana,text="",bg="lightblue")
 nummalo_label.place(x=260,y=230)
 
 def signos():
@@ -33,22 +39,33 @@ def signos():
     else:
         signo_label.config(text="?")
 
-opcion_seleccionada = tk.StringVar()
-opcion1 = tk.Radiobutton(ventana,text="Sumar",variable=opcion_seleccionada,value="Sumar",command=signos)
+opcion_seleccionada = tk.StringVar(value="")
+opcion1 = tk.Radiobutton(ventana,text="Sumar",variable=opcion_seleccionada,value="Sumar",command=signos,bg="lightblue")
 opcion1.place(x=50,y=100)
-opcion2 = tk.Radiobutton(ventana,text="Restar",variable=opcion_seleccionada,value="Restar",command=signos)
+opcion2 = tk.Radiobutton(ventana,text="Restar",variable=opcion_seleccionada,value="Restar",command=signos,bg="lightblue")
 opcion2.place(x=50,y=130)
-opcion3 = tk.Radiobutton(ventana,text="Multiplicar",variable=opcion_seleccionada,value="Multiplicar",command=signos)
+opcion3 = tk.Radiobutton(ventana,text="Multiplicar",variable=opcion_seleccionada,value="Multiplicar",command=signos,bg="lightblue")
 opcion3.place(x=50,y=160)
-opcion4 = tk.Radiobutton(ventana,text="Dividir",variable=opcion_seleccionada,value="Dividir",command=signos)
+opcion4 = tk.Radiobutton(ventana,text="Dividir",variable=opcion_seleccionada,value="Dividir",command=signos,bg="lightblue")
 opcion4.place(x=50,y=190)
 
-dificultad_seleccionada = tk.StringVar(value="Facil")
-dif1 = tk.Radiobutton(ventana,text="Facil",variable=dificultad_seleccionada,value="Facil")
+def reset_contador():
+    global cont,buenas,malas
+    cont = 0
+    buenas = 0
+    malas = 0
+    numjuego_label.config(text="0")
+    numbueno_label.config(text="0")
+    nummalo_label.config(text="0")
+
+dificultad_seleccionada = tk.StringVar(value="")
+dificultad_seleccionada.trace_add('write', reset_contador)
+
+dif1 = tk.Radiobutton(ventana,text="Facil",variable=dificultad_seleccionada,value="Facil",command=reset_contador,bg="lightblue")
 dif1.place(x=30,y=270)
-dif2 = tk.Radiobutton(ventana,text="Medio",variable=dificultad_seleccionada,value="Medio")
+dif2 = tk.Radiobutton(ventana,text="Medio",variable=dificultad_seleccionada,value="Medio",command=reset_contador,bg="lightblue")
 dif2.place(x=100,y=270)
-dif3 = tk.Radiobutton(ventana,text="Dificil",variable=dificultad_seleccionada,value="Dificil")
+dif3 = tk.Radiobutton(ventana,text="Dificil",variable=dificultad_seleccionada,value="Dificil",command=reset_contador,bg="lightblue")
 dif3.place(x=180,y=270)
 
 primer_entry = tk.Entry(ventana, width=10, state='disabled')
@@ -60,52 +77,55 @@ tercer_entry.grid(row=1, column=3, padx=5,pady=15)
 
 def numero():
     dificultad = dificultad_seleccionada.get()
-    if dificultad == "Facil":
-        num1 = random.randint(1,10)
-        num2 = random.randint(1,10)      
-    elif dificultad == "Medio":
-        num1 = random.randint(1,100)
-        num2 = random.randint(1,100)
-    elif dificultad == "Dificil":
-        num1 = random.randint(1,1000)
-        num2 = random.randint(1,1000)
-    primer_entry.config(state='normal')
-    primer_entry.delete(0,tk.END)
-    primer_entry.insert(0,num1)
-    primer_entry.config(state='disabled')
-    segundo_entry.config(state='normal')
-    segundo_entry.delete(0,tk.END)
-    segundo_entry.insert(0,num2)
-    segundo_entry.config(state='disabled')
+    if not dificultad:
+        messagebox.showerror("ATENCIÓN", "Primero tenes que seleccionar una dificultad para jugar!")
+    else:
+        if dificultad == "Facil":
+            num1 = random.randint(1,10)
+            num2 = random.randint(1,10)      
+        elif dificultad == "Medio":
+            num1 = random.randint(1,100)
+            num2 = random.randint(1,100)
+        elif dificultad == "Dificil":
+            num1 = random.randint(1,1000)
+            num2 = random.randint(1,1000)
+        primer_entry.config(state='normal')
+        primer_entry.delete(0,tk.END)
+        primer_entry.insert(0,num1)
+        primer_entry.config(state='disabled')
+        segundo_entry.config(state='normal')
+        segundo_entry.delete(0,tk.END)
+        segundo_entry.insert(0,num2)
+        segundo_entry.config(state='disabled')
 
-cont = 0
-buenas = 0
-malas = 0
 def operacion():
     num1 = int(primer_entry.get())
     num2 = int(segundo_entry.get())
     operacion = opcion_seleccionada.get()
-    if operacion == "Sumar":
-        resultado = num1 + num2
-    elif operacion == "Restar":
-        resultado = num1 - num2
-    elif operacion == "Multiplicar":
-        resultado = num1 * num2
-    elif operacion == "Dividir":
-        resultado = num1 / num2
-    global cont 
-    cont = cont + 1 
-    numjuego_label.config(text=cont)
-    num3 = float(tercer_entry.get())
-    global buenas
-    global malas
-    if num3 == resultado:
-        buenas = buenas + 1
-        numbueno_label.config(text=buenas)
+    if not operacion:
+        messagebox.showerror("ATENCIÓN", "Primero tenes que seleccionar una operación para jugar!")
     else:
-        malas = malas + 1
-        nummalo_label.config(text=malas)
-    tercer_entry.delete(0,tk.END)
+        if operacion == "Sumar":
+            resultado = num1 + num2
+        elif operacion == "Restar":
+            resultado = num1 - num2
+        elif operacion == "Multiplicar":
+            resultado = num1 * num2
+        elif operacion == "Dividir":
+            resultado = num1 / num2
+        global cont 
+        cont = cont + 1 
+        numjuego_label.config(text=cont)
+        num3 = float(tercer_entry.get())
+        global buenas
+        global malas
+        if num3 == resultado:
+            buenas = buenas + 1
+            numbueno_label.config(text=buenas)
+        else:
+            malas = malas + 1
+            nummalo_label.config(text=malas)
+        tercer_entry.delete(0,tk.END)
         
     
 
